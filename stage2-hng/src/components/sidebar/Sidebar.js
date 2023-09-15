64f0a74e1c795b30ccccfd894aa8bc1f786ffc4e
rightone0130 <rightone0130@gmail.com>
2023-09-15 10:21:23 +0100

    working on details page

--- stage2-hng/src/components/sidebar/Sidebar.js
+++ stage2-hng/src/components/sidebar/Sidebar.js
@@ -1,14 +1,14 @@
 import React, { useState, useEffect } from 'react';
-import './Sidebar.css'; // Import your CSS for styling
+import './Sidebar.css';
+import { Link } from "react-router-dom";
 
 const Sidebar = () => {
   const [isActive, setIsActive] = useState(true);
 
   useEffect(() => {
-    // You can set a timeout if you want to delay the activation for some time
-    // setTimeout(() => {
+    setTimeout(() => {
     setIsActive(true);
-    // }, 1000); // Set an optional delay (in milliseconds) if needed
+     }, 1000); 
   }, []);
 
   const toggleSidebar = () => {
@@ -18,17 +18,24 @@ const Sidebar = () => {
   return (
     <div className={`sidebar ${isActive ? 'active' : ''}`}>
       <div className="logo">
-        <img src="/path-to-your-logo.png" alt="Logo" />
+      <Link to="/"><img className="sidebar__header__icon" src="https://res.cloudinary.com/dcntmhgwf/image/upload/v1694503486/Zuri-HNG/Logo_edeb02.svg" /></Link>
       </div>
       <div className="menu">
         <ul>
-          <li>Menu Item 1</li>
-          <li>Menu Item 2</li>
-          <li>Menu Item 3</li>
-          <li>Menu Item 4</li>
+          <li className='home__tab'> <span><img src='https://res.cloudinary.com/dcntmhgwf/image/upload/v1694768520/Zuri-HNG/Home_tam16x.svg'/></span> Home</li>
+          <li className="movies__tab"> <span><img src='https://res.cloudinary.com/dcntmhgwf/image/upload/v1694768520/Zuri-HNG/Movie_Projector_f44era.svg'/></span>Movies</li>
+          <li> <span><img src='https://res.cloudinary.com/dcntmhgwf/image/upload/v1694768520/Zuri-HNG/TV_Show_sjkyuv.svg'/></span>TV Series</li>
+          <li> <span><img src='https://res.cloudinary.com/dcntmhgwf/image/upload/v1694768519/Zuri-HNG/Calendar_s5mkzu.svg'/></span>Upcoming</li>
         </ul>
       </div>
+      <div className="movie__quizes">
+        <p>Play movie quizes <br/>and earn <br/> free tickets</p>
+        <p className='playing__now'>50k people are playing <br/>now</p>
+        <p className='start__playing'>Start playing</p>
+        
+      </div>
       <button className="toggle-button" onClick={toggleSidebar}>
+      <span><img src='https://res.cloudinary.com/dcntmhgwf/image/upload/v1694768519/Zuri-HNG/Logout_zsydqf.svg'/></span>
         Toggle Sidebar
       </button>
     </div>
--- stage2-hng/src/pages/movieDetails/movieDetails.js
+++ stage2-hng/src/pages/movieDetails/movieDetails.js
@@ -79,11 +79,22 @@ const MovieDetails = () => {
 
                             <div className="rated__cantainer">
                                 <span className="top__Rated">Top rated movie #65</span>
+                                <span className="awards__nominations"> Awards 9 nominations</span>
                             </div>
 
                         </div>
                         <div className="moreDetails__left">
-                        {currentMovieDetail ? currentMovieDetail.overview : ""}
+                        <div className="show__times">See Showtimes</div>
+                        <div className="more__watch">More watch options</div>
+                        <div className="more__content">
+                        <div className="movie__poster_container">
+                        <img className="movie__poster_image" src={`https://image.tmdb.org/t/p/original${currentMovieDetail ? currentMovieDetail.poster_path : ""}`} />
+                        <img className="movie__poster_image" src={`https://image.tmdb.org/t/p/original${currentMovieDetail ? currentMovieDetail.poster_path : ""}`} />
+                        <img className="movie__poster_image" src={`https://image.tmdb.org/t/p/original${currentMovieDetail ? currentMovieDetail.poster_path : ""}`} />
+                      
+                        </div>
+                        <div className="best__movies">The Best Movies and Shows in September</div>
+                        </div>
                         </div>
 
                        </div>
@@ -93,67 +104,7 @@ const MovieDetails = () => {
 
               </div>
             
-            <div className="movie__detail">
-                <div className="movie__detailLeft">
-                    {/* <div className="movie__posterBox">
-                        <img className="movie__poster" src={`https://image.tmdb.org/t/p/original${currentMovieDetail ? currentMovieDetail.poster_path : ""}`} />
-                    </div> */}
-                </div>
-                <div className="movie__detailRight">
-                    <div className="movie__detailRightTop">
-                        <div className="movie__name">{currentMovieDetail ? currentMovieDetail.original_title : ""}</div>
-                        <div className="movie__tagline">{currentMovieDetail ? currentMovieDetail.tagline : ""}</div>
-                        <div className="movie__rating">
-                            {currentMovieDetail ? currentMovieDetail.vote_average: ""} <i class="fas fa-star" />
-                            <span className="movie__voteCount">{currentMovieDetail ? "(" + currentMovieDetail.vote_count + ") votes" : ""}</span>
-                        </div>  
-                        <div className="movie__runtime">{currentMovieDetail ? currentMovieDetail.runtime + " mins" : ""}</div>
-                        <div className="movie__releaseDate">{currentMovieDetail ? "Release date: " + currentMovieDetail.release_date : ""}</div>
-                        <div className="movie__genres">
-                            {
-                                currentMovieDetail && currentMovieDetail.genres
-                                ? 
-                                currentMovieDetail.genres.map(genre => (
-                                    <><span className="movie__genre" id={genre.id}>{genre.name}</span></>
-                                )) 
-                                : 
-                                ""
-                            }
-                        </div>
-                    </div>
-                    <div className="movie__detailRightBottom">
-                        <div className="synopsisText">Synopsis</div>
-                        <div>{currentMovieDetail ? currentMovieDetail.overview : ""}</div>
-                    </div>
-                    
-                </div>
-            </div>
-            <div className="movie__links">
-                <div className="movie__heading">Useful Links</div>
-                {
-                    currentMovieDetail && currentMovieDetail.homepage && <a href={currentMovieDetail.homepage} target="_blank" style={{textDecoration: "none"}}><p><span className="movie__homeButton movie__Button">Homepage <i className="newTab fas fa-external-link-alt"></i></span></p></a>
-                }
-                {
-                    currentMovieDetail && currentMovieDetail.imdb_id && <a href={"https://www.imdb.com/title/" + currentMovieDetail.imdb_id} target="_blank" style={{textDecoration: "none"}}><p><span className="movie__imdbButton movie__Button">IMDb<i className="newTab fas fa-external-link-alt"></i></span></p></a>
-                }
-            </div>
-            {/* <div className="movie__heading">Production companies</div> */}
-            {/* <div className="movie__production">
-                {
-                    currentMovieDetail && currentMovieDetail.production_companies && currentMovieDetail.production_companies.map(company => (
-                        <>
-                            {
-                                company.logo_path 
-                                && 
-                                <span className="productionCompanyImage">
-                                    <img className="movie__productionComapany" src={"https://image.tmdb.org/t/p/original" + company.logo_path} />
-                                    <span>{company.name}</span>
-                                </span>
-                            }
-                        </>
-                    ))
-                }
-            </div> */}
+            
         </div>
     )
 }
